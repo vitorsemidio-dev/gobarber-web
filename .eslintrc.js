ca6db1a6686a5a1c49bcbb5cc0b056104dae76d2
Vitor Emidio <vitorsemidio.96@gmail.com>
2020-01-22 00:33:29 -0300

    Importação e verificação de layout utilizado

--- .eslintrc.js
+++ .eslintrc.js
@@ -31,5 +31,6 @@ module.exports = {
     'no-console': ['error', { allow: ['tron'] }],
     'react-hooks/rules-of-hooks': 'error',
     'react-hooks/exhaustive-deps': 'warn',
+    'react/jsx-props-no-spreading': 'off',
   }
 }
