a873cedcbec3fd73b30beb542e296b8288bc7d04
Vitor Emidio <vitorsemidio.96@gmail.com>
2020-01-23 07:55:23 -0300

    Importação logo e estrutura SingIn

--- src/pages/SingIn/index.js
+++ src/pages/SingIn/index.js
@@ -1,7 +1,21 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
+import logo from '~/assets/logo.svg';
 // import { Container } from './styles';
 
 export default function SingIn() {
-  return <h1>SingIn</h1>;
+  return (
+    <>
+      <img src={logo} alt="Logo GoBarber" />
+
+      <form action="">
+        <input type="email" placeholder="Seu e-mail" />
+        <input type="password" placeholder="Sua senha" />
+
+        <button type="submit">Acessar</button>
+        <Link to="/register">Criar conta gratuita</Link>
+      </form>
+    </>
+  );
 }
