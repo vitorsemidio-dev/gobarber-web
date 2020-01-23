5eb2f369006f9700872a42df2483d3685def5132
Vitor Emidio <vitorsemidio.96@gmail.com>
2020-01-23 18:55:23 -0300

    Estrutura tela SingUp

--- src/pages/SingUp/index.js
+++ src/pages/SingUp/index.js
@@ -1,7 +1,21 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-// import { Container } from './styles';
+import logo from '~/assets/logo.svg';
 
 export default function SingUp() {
-  return <div />;
+  return (
+    <>
+      <img src={logo} alt="Logo GoBarber" />
+
+      <form>
+        <input placeholder="Nome Completo" />
+        <input type="email" placeholder="Seu e-mail" />
+        <input type="password" placeholder="Sua senha" />
+
+        <button type="submit">Criar Conta</button>
+        <Link to="/">Já tenho login</Link>
+      </form>
+    </>
+  );
 }
