656bb3dc6b025c1bfe7e365f3393e7119a64f134
Vitor Emidio <vitorsemidio.96@gmail.com>
2020-01-23 21:14:49 -0300

    Utilização biblioteca unform da Rocketseat

--- src/pages/SingUp/index.js
+++ src/pages/SingUp/index.js
@@ -1,21 +1,25 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
 
 import logo from '~/assets/logo.svg';
 
 export default function SingUp() {
+  function handleSumit(data) {
+    console.tron.log(data);
+  }
   return (
     <>
       <img src={logo} alt="Logo GoBarber" />
 
-      <form>
-        <input placeholder="Nome Completo" />
-        <input type="email" placeholder="Seu e-mail" />
-        <input type="password" placeholder="Sua senha" />
+      <Form onSubmit={handleSumit}>
+        <Input name="name" placeholder="Nome Completo" />
+        <Input name="email" type="email" placeholder="Seu e-mail" />
+        <Input name="password" type="password" placeholder="Sua senha" />
 
         <button type="submit">Criar Conta</button>
         <Link to="/">Já tenho login</Link>
-      </form>
+      </Form>
     </>
   );
 }
