a25d61dbb5f3b2593a997d79d9d11f8d832c4c52
Vitor Emidio <vitorsemidio.96@gmail.com>
2020-01-25 15:08:50 -0300

    PersistGame no App

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import { PersistGate } from 'redux-persist/integration/react';
 import { Provider } from 'react-redux';
 import { Router } from 'react-router-dom';
 
 import './config/ReactotronConfig';
-import { store } from './store';
+import { store, persistor } from './store';
 
 import GlobalStyle from './styles/global';
 
@@ -13,10 +14,12 @@ import history from './services/history';
 function App() {
   return (
     <Provider store={store}>
-      <Router history={history}>
-        <Routes />
-        <GlobalStyle />
-      </Router>
+      <PersistGate persistor={persistor}>
+        <Router history={history}>
+          <Routes />
+          <GlobalStyle />
+        </Router>
+      </PersistGate>
     </Provider>
   );
 }
