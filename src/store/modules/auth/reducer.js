7462b789fb4968bad3abaffbb76524a1e09ba762
Vitor Emidio <vitorsemidio.96@gmail.com>
2020-01-25 15:17:54 -0300

    Adicionado reducers para sign_in_request e failure

--- src/store/modules/auth/reducer.js
+++ src/store/modules/auth/reducer.js
@@ -8,10 +8,20 @@ const INITIAL_STATE = {
 
 export default function auth(state = INITIAL_STATE, action) {
   switch (action.type) {
+    case '@auth/SIGN_IN_REQUEST':
+      return produce(state, draft => {
+        draft.loading = true;
+      });
     case '@auth/SIGN_IN_SUCCESS':
       return produce(state, draft => {
         draft.token = action.payload.token;
         draft.signed = true;
+        draft.loading = false;
+      });
+    case '@auth/SIGN_FAILURE':
+      return produce(state, draft => {
+        draft.profile = action.payload.user;
+        draft.loading = false;
       });
     default:
       return state;
