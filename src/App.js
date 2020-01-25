60f6d8068297c783c5ac921f361f2a9384e059d4
Vitor Emidio <vitorsemidio.96@gmail.com>
2020-01-25 15:07:11 -0300

    Adicionado redux-persist ao index store e correção importação

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { Provider } from 'react-redux';
 import { Router } from 'react-router-dom';
 
 import './config/ReactotronConfig';
-import store from './store';
+import { store } from './store';
 
 import GlobalStyle from './styles/global';
 
