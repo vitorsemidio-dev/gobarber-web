c2c1fcbcc4129b2450db468c13598058f31720fc
Vitor Emidio <vitorsemidio.96@gmail.com>
2020-01-26 22:30:42 -0300

    Set token como defaults para api

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
+import api from '~/services/api';
 
 // import { Container } from './styles';
 
 export default function Dashborard() {
+  api.get('appointments');
   return <h1>Dashboard</h1>;
 }
