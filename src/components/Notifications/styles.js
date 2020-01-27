094e825f6c53c2866dd4cd4a0beeb74cabd85f08
Vitor Emidio <vitorsemidio.96@gmail.com>
2020-01-26 23:22:14 -0300

    Estilização badge e prop hasnunread

--- src/components/Notifications/styles.js
+++ src/components/Notifications/styles.js
@@ -1,5 +1,26 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  position: relative;
+`;
 
-export const Badge = styled.button``;
+export const Badge = styled.button`
+  background: none;
+  border: 0;
+  position: relative;
+
+  ${props =>
+    props.hasUnread &&
+    css`
+      &::after {
+        position: absolute;
+        right: 0;
+        top: 0;
+        width: 8px;
+        height: 8px;
+        background: #ff892e;
+        content: '';
+        border-radius: 50%;
+      }
+    `}
+`;
