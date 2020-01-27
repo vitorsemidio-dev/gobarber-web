c9018b55c2939f95c85f0e9a5e31bd1d0c06158a
Vitor Emidio <vitorsemidio.96@gmail.com>
2020-01-27 20:42:58 -0300

    Estilização botões

--- src/pages/Profile/styles.js
+++ src/pages/Profile/styles.js
@@ -24,6 +24,13 @@ export const Container = styled.div`
       }
     }
 
+    hr {
+      border: 0;
+      height: 1px;
+      background: rgba(255, 255, 255, 0.2);
+      margin: 10px 0 20px;
+    }
+
     button {
       margin: 5px 0 0;
       height: 44px;
@@ -36,7 +43,7 @@ export const Container = styled.div`
       transition: background 0.2s;
 
       &:hover {
-        background: ${darken(0.03, '#3b9eff')};
+        background: ${darken(0.08, '#3b9eff')};
       }
     }
 
@@ -56,6 +63,23 @@ export const Container = styled.div`
       align-self: flex-start;
       margin: 0 0 10px;
       font-weight: bold;
+    }
+  }
+
+  > button {
+    width: 100%;
+    margin: 10px 0 0;
+    height: 44px;
+    background: #f64c75;
+    font-weight: bold;
+    color: #fff;
+    border: 0;
+    border-radius: 4px;
+    font-size: 16px;
+    transition: background 0.2s;
+
+    &:hover {
+      background: ${darken(0.08, '#F64C75')};
     }
   }
 `;
