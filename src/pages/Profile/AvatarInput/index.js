db511cf6badef2ea064762005331c10b82302655
Vitor Emidio <vitorsemidio.96@gmail.com>
2020-01-28 06:49:33 -0300

    implementação função handleChange

--- src/pages/Profile/AvatarInput/index.js
+++ src/pages/Profile/AvatarInput/index.js
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
 import { useField } from '@rocketseat/unform';
+
+import api from '~/services/api';
 
 import { Container } from './styles';
 
 export default function AvatarInput() {
-  function handleChange(e) {}
+  const { defaultValue, registerField } = useField('avatar');
+  const [file, setFile] = useState(defaultValue && defaultValue.id);
+  const [preview, setPreview] = useState(defaultValue && defaultValue.url);
+
+  const ref = useRef();
+
+  async function handleChange(e) {
+    const data = new FormData();
+
+    data.append('file', e.target.files[0]);
+
+    const { id, url } = await api.post('files', data);
+
+    setFile(id);
+    setPreview(url);
+  }
+
   return (
     <Container>
       <label htmlFor="avatar">
@@ -15,6 +33,8 @@ export default function AvatarInput() {
           onChange={handleChange}
           type="file"
           id="avatar"
+          data-file={file}
+          ref={ref}
         />
       </label>
     </Container>
