00fc6fbee1a549f32e9ddf0a10bea4f89dee6215
Vitor Emidio <vitorsemidio.96@gmail.com>
2020-01-27 21:50:45 -0300

    Adição elementos no AvatarInput

--- src/pages/Profile/AvatarInput/index.js
+++ src/pages/Profile/AvatarInput/index.js
@@ -1,7 +1,22 @@
 import React from 'react';
+import { useField } from '@rocketseat/unform';
 
-// import { Container } from './styles';
+import { Container } from './styles';
 
 export default function AvatarInput() {
-  return <div />;
+  function handleChange(e) {}
+  return (
+    <Container>
+      <label htmlFor="avatar">
+        <img src="" alt="" />
+
+        <input
+          accept="image/*"
+          onChange={handleChange}
+          type="file"
+          id="avatar"
+        />
+      </label>
+    </Container>
+  );
 }
