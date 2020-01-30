23264b6ee4eab667ae73866c8fa395edc0e06288
Vitor Emidio <vitorsemidio.96@gmail.com>
2020-01-29 21:19:07 -0300

    Mostrando data atual do sistema

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,17 +1,25 @@
-import React from 'react';
+import React, { useState, useMemo } from 'react';
+import { format } from 'date-fns';
+import pt from 'date-fns/locale/pt';
 import { MdChevronLeft, MdChevronRight } from 'react-icons/md';
 
 import { Container, Time } from './styles';
 import api from '~/services/api';
 
 export default function Dashboard() {
+  const [date, setDate] = useState(new Date());
+
+  const dateFormatted = useMemo(
+    () => format(date, "d 'de' MMMM", { locale: pt }),
+    [date]
+  );
   return (
     <Container>
       <header>
         <button type="button">
           <MdChevronLeft size={36} color="#FFF" />
         </button>
-        <strong>29 de janeiro</strong>
+        <strong>{dateFormatted}</strong>
         <button type="button">
           <MdChevronRight size={36} color="#FFF" />
         </button>
